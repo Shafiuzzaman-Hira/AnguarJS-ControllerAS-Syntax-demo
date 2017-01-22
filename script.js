22b8514c50d4336a07ed2823dad30a71e6eb9121
Md. Shafiuzzaman Hira <md.shafiuzzaman.hira@gmail.com>
2017-01-22 14:56:30 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// App
+(function() {
+  'use strict';
+
+  angular
+    .module('testModule', []);
+})();
+
+//Controller
+(function() {
+  'use strict';
+
+  angular
+    .module('testModule')
+    .controller('testController', testController);
+
+  function testController() {
+    var testModel = this;
+    testModel.name = 'Hira';
+    testModel.profession = 'Software Engineer';
+    testModel.company = 'Mobil Bangladesh';
+  }
+
+})();
